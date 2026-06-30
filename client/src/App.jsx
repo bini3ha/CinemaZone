01f41399155aa7f524c61372876da3c49f052a0c
bini3ha <tamrakarbinisha9@gmail.com>
2026-06-30 10:04:46 +0545

    Admin Navbar and Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,11 @@ import MyBookings from './Pages/MyBookings'
 import Favorite from './Pages/Favorite'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './Pages/admin/Layout'
+import Dashboard from './Pages/admin/Dashboard'
+import Addshows from './Pages/admin/Addshows'
+import ListShows from './Pages/admin/ListShows'
+import ListBookings from './Pages/admin/ListBookings'
 
 const App = () => {
 
@@ -17,14 +22,20 @@ const App = () => {
   return (
     <>
       <Toaster />
-      {!isAdminRoute && <Navbar/>}
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/movies' element={<Movies/>} />
-        <Route path='/movies/:id' element={<MovieDetails/>} />
-        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
-        <Route path='/my-bookings' element={<MyBookings/>} />
-        <Route path='/favorite' element={<Favorite/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-shows' element={<Addshows />} />
+          <Route path='list-shows' element={<ListShows />} />
+          <Route path='list-bookings' element={<ListBookings />} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
