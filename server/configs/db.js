e6443570db1d3ceb5aecccad679bece7217e31cd
bini3ha <tamrakarbinisha9@gmail.com>
2026-07-01 00:23:43 +0545

    model createFunction fixed

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () =>{
     try {
-        mongoose.connection.on('connected', ()=> console.log('Datbase connected'));
+        mongoose.connection.on('connected', ()=> console.log('Database connected'));
         await mongoose.connect(`${process.env.MONGODB_URI}/cinemazone`)
     } catch (error) {
         console.log(error.message);
